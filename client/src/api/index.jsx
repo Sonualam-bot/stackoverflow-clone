3d385d7bb1bdf224acb59e62a0741e18812ee3b8
Sonualam <sonualam1599@gmail.com>
2024-01-02 19:35:04 +0530

    fix: updated hosted backend url

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://stackoverflow-clone-backend-sonu-api.vercel.app",
+});
 
 API.interceptors.request.use(
   (req) => {
